74ae3942320a3ccff93e3d12362e210bd04d5fb2
melina-thegarza <mjg2290@columbia.edu>
2022-12-28 00:03:47 -0600

    added missing dependencies

---
+++ frontend/build/precache-manifest.cc125090cd96cda74e13337fa3f4d56c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a891c0b7a4b66d29da47",
+    "url": "/static/css/main.2018b112.chunk.css"
+  },
+  {
+    "revision": "a891c0b7a4b66d29da47",
+    "url": "/static/js/main.a891c0b7.chunk.js"
+  },
+  {
+    "revision": "753cd6d43131dacd180a",
+    "url": "/static/css/1.dcc676cb.chunk.css"
+  },
+  {
+    "revision": "753cd6d43131dacd180a",
+    "url": "/static/js/1.753cd6d4.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "abc00b7e5f11bb0f697800a4a9d9c430",
+    "url": "/index.html"
+  }
+];
